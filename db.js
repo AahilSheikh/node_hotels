a76b0506ef3da0e0d83a08dbfe67b7bc1b7b45b6
aahil <aahilsheikh1711@gmail.com>
2025-01-20 12:55:17 +0530

    add update code

--- db.js
+++ db.js
@@ -1,31 +1,27 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoURL = process.env.MONGODB_URL_LOCAL;
+// Use MongoDB URL from the .env file
+const mongoURL = process.env.MONGODB_URL || 'mongodb+srv://aahil:aahil786@chat.2qv54.mongodb.net/mydatabase?retryWrites=true&w=majority';
 
-mongoose.connect(mongoURL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-
-
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURL)
+    .then(() => console.log('Connected to MongoDB Atlas server'))
+    .catch(err => console.error('MongoDB Atlas connection error:', err));
 
 const db = mongoose.connection;
 
+// MongoDB events
 db.on('connected', () => {
-    console.log('Connected to MongoDB server');
-    
-})
+    console.log('MongoDB connection established');
+});
 
 db.on('error', (err) => {
-    console.error('MongoDB connection error:',err);
-    
-})
+    console.error('MongoDB error:', err);
+});
 
 db.on('disconnected', () => {
-    console.log(' MongoDB disconnected');
-    
-})
+    console.log('MongoDB disconnected');
+});
 
 module.exports = db;
